6634bd44e2ddc0c40000e8b8c844924845bea97c
Alex H. <alexcp.hernandez@gmail.com>
2025-02-07 09:46:37 -0600

    Reorganized folder structure. Began page 7 notes AI-102

---
+++ update_git_config_personal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update Git global configuration
+git config --global user.name "Alex H."
+git config --global user.email "alexcp.hernandez@gmail.com"
+
+git config --list
---
+++ update_git_config_work.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update Git global configuration
+git config --global user.name "Alex Hernandez"
+git config --global user.email "ahernandez@alixpartners.com"
+
+git config --list
